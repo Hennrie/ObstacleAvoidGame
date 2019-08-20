bc2d7646e7d8b5b6cdec36c5e15eb4411c1885db
Henning K <horst91@gmx.net>
2019-08-20 23:33:55 +0200

    initialize project files

---
+++ core/src/com/obstacleavoid/entity/Player.java
@@ -0,0 +1,15 @@
+package com.obstacleavoid.entity;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.obstacleavoid.config.GameConfig;
+
+public class Player extends GameObjectClass {
+
+    public Player() {
+
+        super(GameConfig.PLAYER_BOUNDS_RADIUS);
+        setSize(GameConfig.PLAYER_SIZE, GameConfig.PLAYER_SIZE);
+    }
+
+}
---
+++ desktop/src/com/obstacleavoid/desktop/AssetPacker.java
@@ -0,0 +1,34 @@
+package com.obstacleavoid.desktop;
+
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
+public class AssetPacker {
+
+    public static final boolean DRAW_DEBUG_OUTLINE = false;
+
+    public static final String RAW_ASSETS_PATH = "desktop/assets-raw";
+    public static final String ASSETS_PATH = "android/assets";
+
+    public static void main(String[] args) {
+        TexturePacker.Settings settings = new TexturePacker.Settings();
+        settings.debug = DRAW_DEBUG_OUTLINE;
+
+
+        TexturePacker.process(settings,
+                RAW_ASSETS_PATH + "/gameplay",
+                ASSETS_PATH + "/gameplay",
+                "gameplay");
+
+        TexturePacker.process(RAW_ASSETS_PATH + "/ui",
+                ASSETS_PATH + "/ui",
+                "ui");
+
+        TexturePacker.process(settings,
+                RAW_ASSETS_PATH + "/skin",
+                ASSETS_PATH + "/ui",
+                "uiskin");
+
+    }
+
+
+}
